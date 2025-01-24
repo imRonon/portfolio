96be0682623578cfbd5d6bee8f8ddcb67845198a
imRonon <kotopocen@gmail.com>
2025-01-24 09:46:44 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    items: 1.3,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    nav: true,
+});
